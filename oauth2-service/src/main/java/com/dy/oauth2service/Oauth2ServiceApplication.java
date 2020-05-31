06f67061b09d5c120f75e70c83c9c4379a3afb6e
Yezhong <yezhong.zhang@endowus.com>
2020-05-31 23:43:21 +0800

    upgrade spring boot: 最高无破坏性可升级到2.0.9, 2.1.0后默认无自动overriding (highest version upgrade without destruction, default no overriding after 2.1.0)

--- oauth2-service/src/main/java/com/dy/oauth2service/Oauth2ServiceApplication.java
+++ oauth2-service/src/main/java/com/dy/oauth2service/Oauth2ServiceApplication.java
@@ -2,6 +2,7 @@ package com.dy.oauth2service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableEurekaClient
 @EnableJpaAuditing
 @EnableResourceServer
+@EnableDiscoveryClient
 public class Oauth2ServiceApplication {
 
     public static void main(String[] args) {
